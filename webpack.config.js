9dc44d86de5c0764b227e8bd807ddc61b092df0e
Chilly Orange <mattijs@chillyorange.com>
2019-04-26 13:23:33 +0700

    Colors

--- webpack.config.js
+++ webpack.config.js
@@ -3,29 +3,51 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-  entry: './src/styles.css',
-  mode: process.env.NODE_ENV,
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: [
-            { loader: 'css-loader', options: { importLoaders: 1 } },
-            'postcss-loader',
-          ],
-        }),
-      },
-    ],
-  },
-  plugins: [
-    new ExtractTextPlugin('styles.css', {
-      disable: process.env.NODE_ENV === 'development',
-    }),
-    new HtmlWebpackPlugin({
-      filename: 'index.html',
-      template: 'src/index.html',
-    }),
-  ],
+	entry: './src/styles.css',
+	mode: process.env.NODE_ENV,
+	module: {
+		rules: [
+			{
+				test: /\.css$/,
+				use: ExtractTextPlugin.extract({
+					fallback: 'style-loader',
+					use: [
+						{ loader: 'css-loader', options: { importLoaders: 1 } },
+						'postcss-loader',
+					],
+				}),
+			},
+			{
+	            test: /\.scss$/,
+	            use: [
+	                "style-loader",
+	                "css-loader",
+	                "sass-loader"
+	            ]
+	        },
+	        {
+	            test: /\.(png|jp(e*)g|svg)$/,  
+	            use: [{
+	                loader: 'url-loader',
+	                options: { 
+	                    limit: 20000, // Convert images < 8kb to base64 strings
+	                    name: 'images/[name].[ext]'
+	                } 
+	            }]
+        }
+		],
+	},
+	plugins: [
+		new ExtractTextPlugin('styles.css', {
+			disable: process.env.NODE_ENV === 'development',
+		}),
+		new HtmlWebpackPlugin({
+			filename: 'index.html',
+			template: 'src/index.html',
+		}),
+		new HtmlWebpackPlugin({
+			filename: 'colors.html',
+			template: 'src/colors.html',
+		}),
+	],
 }
